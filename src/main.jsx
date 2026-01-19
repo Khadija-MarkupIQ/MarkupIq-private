dd166a501f01fed905fc3864b83a7747bef19af7
Dhanush <dhanush57678@gmail.com>
2026-01-19 17:04:20 -0500

    Log VITE_API_BASE_URL in frontend entry (Vite)

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
 import './index.css'
-import './App.css'
-import App from './App.jsx'
+
 console.log("VITE_API_BASE_URL =", import.meta.env.VITE_API_BASE_URL);
-<<<<<<< HEAD
 
-=======
->>>>>>> main
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>
 )
